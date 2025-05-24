13c65bcc438c47af1469b08caae635cc804f97f4
Bert Wagner <bertwagner@bertwagner.com>
2025-05-23 20:27:56 -0400

    gets captive portal popup working

---
+++ src/CaptivePortal.h
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+// Captive Portal
+#include <AsyncTCP.h>  //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
+#include <DNSServer.h>
+#include <ESPAsyncWebServer.h>	//https://github.com/me-no-dev/ESPAsyncWebServer using the latest dev version from @me-no-dev
+#include <esp_wifi.h>			//Used for mpdu_rx_disable android workaround
+
+
+class CaptivePortal {
+    public:
+        CaptivePortal();
+
+        void init();
+        void processNextDNSRequest();
+
+    private:
+        void setUpDNSServer(DNSServer &dnsServer, const IPAddress &localIP);
+        void startSoftAccessPoint(const char *ssid, const char *password, const IPAddress &localIP, const IPAddress &gatewayIP);
+        void setUpWebserver(AsyncWebServer &server, const IPAddress &localIP);
+};
